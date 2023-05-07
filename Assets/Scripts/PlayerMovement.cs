640aa1566a4be991c6c2256526fac154e2d8c8d5
Reveck <cgngtr5026@hotmail.com>
2023-05-07 18:08:30 +0300

    Idle & jump scripts are added.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public PlayerController _playerController;
+    public Animator animator;
     float horizontalMove = 0f;
     public float runSpeed = 40f;
     [Range(0, 1000f)][SerializeField] private float JumpPower = 400f;
     [SerializeField] private int JumpCount;
     [Range(0, 10)][SerializeField] private int _MaxJumpCount;
+    
 
     void Start()
     {
@@ -19,11 +21,15 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        
+
         if (Input.GetButtonDown("Jump"))
         {
+            animator.SetBool("IsJumping", true);
             if (_playerController._Grounded == true)
             {
                 JumpCount = _MaxJumpCount;
+                animator.SetBool("IsJumping", false);
             }
 
             if (JumpCount <= _MaxJumpCount - 1 && JumpCount > 0) {
@@ -42,6 +48,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         _playerController.Move(horizontalMove * Time.deltaTime);
     }
 }
